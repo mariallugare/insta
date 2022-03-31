58a24e5a6497761f20cf64cba89c2390939af209
Marial Clarie <marialclarie@gmail.com>
2022-03-31 15:43:07 +0300

    create views on views.py

--- instaclone/insta/models.py
+++ instaclone/insta/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
-class post(models.Models):
+class post(models.Model):
     author = models.ForeignKey('auth.user',on_delete =models.CASCADE)
     image = models.ImageField(blank=True,null=True)
     caption = models.TextField()
     created_date =models.DateTimeField(default= timezone.now)
+
+    def __str__(self):
+        return self.caption
--- instaclone/insta/views.py
+++ instaclone/insta/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import post
+from django.views.generic import (ListView)
 # Create your views here.
+
+class PostisView(ListView):
+    template_name = "insta/post_lists.html"
+    Queryset =post.objects.all()
+    context_object_name ='posts'
