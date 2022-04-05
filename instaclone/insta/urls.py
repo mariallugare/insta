03aa25c96080a48bfad94e903a9a497a33c24e6a
Marial Clarie <marialclarie@gmail.com>
2022-04-05 18:43:48 +0300

    create login page

--- instaclone/insta/urls.py
+++ instaclone/insta/urls.py
@@ -7,7 +7,5 @@ app_name ='insta'
 #local: http://127.0.0.1:8000/
 urlpatterns= [
      path('post/', PostListView.as_view(), name="post_list"),
-     path('', views.index, name='home')
-  
-
+     path('', views.index, name='home'),
 ]
